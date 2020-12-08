cba7d9a851c195be33a17c3e3514d328ce62d033
CasperMobit <casper.tollefsen@mobit.no>
2020-12-08 17:38:32 +0100

    Restrutured file system

---
+++ Build/languages/index.php
@@ -0,0 +1,10 @@
+<?php include('http://localhost:3000/modules/stdSoF.html');?>
+<title>HTML</title>
+<?php include('http://localhost:3000/modules/stdHead.html');?>
+<?php include('http://localhost:3000/modules/header.html');?>
+
+<div class="container wrapper">
+    <h1>HyperTextMarkupLanguage</h1>
+</div>
+
+<?php require('http://localhost:3000/modules/stdEoF.html'); ?>
