0a664eda151b8e686bc1ea2aeb815ff84155b25b
CasperMobit <casper.tollefsen@mobit.no>
2020-12-17 14:27:57 +0100

    Further implementation of own responsiveness

--- layout.php
+++ layout.php
@@ -3,51 +3,66 @@
 <head>
 	<title>Bootstrap</title>
 	<?php require('components/headContent.html');?>
-	<link href="./css/layout-test.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
 
 <?php include('./components/header.html'); ?>
 
 <div class="row" id="wrapper">
-	<div class="col-3"></div>
+	<div class="col-3 empty"></div>
 	<div class="col-6 content">
-		<div class="row">
-			<div class="col-1"></div>
-			<div class="col-10">
-				<h1>Layout</h1>
-				<p class="meta">This layout was made using CSS only!</p>
-			</div>
-			<div class="col-1"></div>
-		</div>
-		<div class="row">
-			<div class="col-1"></div>
-			<div class="col-8">
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-			</div>
-			<div class="col-3"></div>
-		</div>
+		<h1>Layout</h1>
+		<p class="meta">This layout was made using CSS only!</p>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		<h2>Size Guide</h2>
+		<table>
+			<tr>
+				<th>Value</th>
+				<th>Device</th>
+				<th>Size</th>
+			</tr>
+			<tr>
+				<td>xs</td>
+				<td>Phones</td>
+				<td>Screens less than 768px wide</td>
+			</tr>
+			<tr>
+				<td>xs</td>
+				<td>Phones</td>
+				<td>Screens less than 768px wide</td>
+			</tr>
+			<tr>
+				<td>xs</td>
+				<td>Phones</td>
+				<td>Screens less than 768px wide</td>
+			</tr>
+			<tr>
+				<td>xs</td>
+				<td>Phones</td>
+				<td>Screens less than 768px wide</td>
+			</tr>
+		</table>
 	</div>
-	<div class="col-3"></div>
+	<div class="col-3 empty"></div>
 </div>
-<div class="row debug">
+<div class="row">
 	<div class="col-2"></div>
 	<div class="col-3"></div>
 	<div class="col-5"></div>
 	<div class="col-2"></div>
 </div>
-<div class="row debug">
+<div class="row">
 	<div class="col-4"></div>
 	<div class="col-4"></div>
 	<div class="col-4"></div>
 </div>
-<div class="row debug">
+<div class="row">
 	<div class="col-3"></div>
 	<div class="col-3"></div>
 	<div class="col-3"></div>
 	<div class="col-3"></div>
 </div>
-<div class="row debug">
+<div class="row">
 	<div class="col-2"></div>
 	<div class="col-2"></div>
 	<div class="col-2"></div>
@@ -55,7 +70,7 @@
 	<div class="col-2"></div>
 	<div class="col-2"></div>
 </div>
-<div class="row debug">
+<div class="row">
 	<div class="col-1"></div>
 	<div class="col-1"></div>
 	<div class="col-1"></div>
@@ -69,7 +84,7 @@
 	<div class="col-1"></div>
 	<div class="col-1"></div>
 </div>
-<div class="row debug">
+<div class="row">
 	<div class="col-2"></div>
 	<div class="col-10"></div>
 </div>
