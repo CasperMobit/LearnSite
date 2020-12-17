4aa7e24c4c898798b586e76d6fdbc8106cf15835
CasperMobit <casper.tollefsen@mobit.no>
2020-12-17 23:46:13 +0100

    Savepoint

--- layout.php
+++ layout.php
@@ -9,8 +9,8 @@
 <?php include('./components/header.html'); ?>
 
 <div class="row" id="wrapper">
-	<div class="col-3"></div>
-	<div class="col-6 content">
+	<div class="col-2"></div>
+	<div class="col-8 content">
 		<h1>Layout</h1>
 		<p class="meta">This layout was made using CSS only!</p>
 		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
@@ -43,7 +43,7 @@
 			</tr>
 		</table>
 	</div>
-	<div class="col-3"></div>
+	<div class="col-2"></div>
 </div>
 <div class="row">
 	<div class="col-2"></div>
