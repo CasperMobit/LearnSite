8039dcf33a5efb7185398300febf121782f1c93b
CasperMobit <casper.tollefsen@mobit.no>
2020-12-25 17:48:29 +0100

    Added JS functionality

---
+++ javascript.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>JavaScript</title>
+	<?php require('components/headContent.html');?>
+</head>
+<body>
+
+<?php include('./components/header.html'); ?>
+
+<div class="row" id="wrapper">
+	<div class="col-3"></div>
+	<div class="col-6 content">
+		<h1>Javascript</h1>
+		<script src="js/add-content.js"></script>
+	</div>
+	<div class="col3"></div>
+</div>
+
+</body>
