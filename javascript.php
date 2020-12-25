d9debc9350fe80f6656b57c747c5332174ce871e
CasperMobit <casper.tollefsen@mobit.no>
2020-12-25 18:50:29 +0100

    Added JS code to display current date and time

--- javascript.php
+++ javascript.php
@@ -13,6 +13,39 @@
 	<div class="col-6 content">
 		<h1>Javascript</h1>
 		<script src="js/add-content.js"></script>
+		<script>
+			document.write('<p>This is another way to add HTML using JavaScript</p>');
+
+			var time = new Date();
+			var weekDay = time.getDay();
+			var day = time.getDate();
+			var month = time.getMonth() + 1;
+			var year = time.getFullYear();
+			var hour = time.getHours();
+			var minute = time.getMinutes();
+
+			// Replace weekDay number with name of the day
+			if (weekDay == 1) {
+				weekDay = 'Monday';
+			} else if (weekDay == 2) {
+				weekDay = 'Tuesday';
+			} else if (weekDay == 3) {
+				weekDay = 'Wednesday';
+			} else if (weekDay == 4) {
+				weekDay = 'Thursday';
+			} else if (weekDay == 5) {
+				weekDay = 'Friday';
+			} else if (weekDay == 6) {
+				weekDay = 'Saturday';
+			} else if (weekDay == 7) {
+				weekDay = 'Sunday';
+			} else {
+				weekDay = 'Date:'
+			}
+
+			// Display the date as "Name DD.MM.YY HH:mm"
+			document.write('<p>' + weekDay + ' ' + day + '.' + month + '.' + year + ' ' + hour + ':' + minute + '</p>');
+		</script>
 	</div>
 	<div class="col3"></div>
 </div>
