3c9e7808a9ac7fec31a7e0a70210f8063b9d6fbe
CasperMobit <casper.tollefsen@mobit.no>
2020-12-26 15:07:18 +0100

    Added API Guide page

---
+++ api.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>LearnSite | API</title>
+	<?php require('components/headContent.html');?>
+</head>
+<body>
+
+<?php include('./components/header.html'); ?>
+
+<div class="row" id="wrapper">
+    <div class="col-2"></div>
+    <div class="col-8 content">
+
+        <h1>API Guide</h1>
+        
+    </div>
+    <div class="col-2"></div>
+</div>    
+
+</body>
+</html>
