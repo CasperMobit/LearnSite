bc093263381a67a7e87ef6fc50b67c5776188b91
CasperMobit <casper.tollefsen@mobit.no>
2020-12-27 16:41:11 +0100

    Added content to JS page

--- javascript.php
+++ javascript.php
@@ -11,18 +11,27 @@
 <div class="row" id="wrapper">
 	<div class="col-2"></div>
 	<div class="col-8 content">
+
 		<h1>Javascript</h1>
-		<h2>Creating Models Using Data</h2>
+		<h2>Contents</h2>
+		<ul class="noStyle smallIndent">
+			<li><a href="#contCreatingModelsUsingData">Creating Models Using Data</a></li>
+			<li><a href="#contExamples">Examples</a></li>
+		</ul>
+
+		<h2 id="contCreatingModelsUsingData">Creating Models Using Data</h2>
+
 		<h3>Objects &amp; Properties</h3>
 		<p>In programming, each physical thing in the world can be represented as an <strong>object</strong>.<br/>There can be several <strong>instances</strong> of an object.</p>
-		<p>Each object can have its own:</p>
-		<ul>
+		<p style="margin-bottom: 0;">Each object can have its own:</p>
+		<ul style="margin-top: 0; padding-left: 1.1em;">
 			<li>Properties</li>
 			<li>Events</li>
 			<li>Methods</li>
 		</ul>
+		<p>An object is described using <strong>properties</strong>.</p>
 
-		<h2>Examples</h2>
+		<h2 id="contExamples">Examples</h2>
 		<script src="js/add-content.js"></script>
 		<script>
 			document.write('<p>You can use the "document.write()" method to add HTML using JavaScript</p>');
