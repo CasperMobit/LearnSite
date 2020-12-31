c1ad4972f064cb1914cf9c06fdbfc207054aaca9
CasperMobit <casper.tollefsen@mobit.no>
2020-12-31 17:37:49 +0100

    Added content to Encoder page

--- apps/htmlencoder.php
+++ apps/htmlencoder.php
@@ -13,14 +13,17 @@
     <div class="col-8 content">
 
         <h1>HTML Encoder</h1>
+        <p class="meta">Converts text to escaped HTML code</p>
         <p>Input the text you want to encode:</p>
         <textarea id="htmlCodeInput" rows="10">Type here...</textarea>
         <button id="submitHtmlBtn" style="margin-top:.6rem;">Encode</button>
 
         <div style="margin-top:3rem;">
             <h3>Result</h3>
-            <div id="htmlEncodeResult" style="overflow-wrap:break-word;"><p>This is the result</p></div>
+            <div id="htmlEncodeResult" style="overflow-wrap:break-word;"><p>Result is displayed here</p></div>
         </div>
+
+        <p style="font-size:.8rem; margin-top:6rem;">View the complete ASCII table at <a href="https://www.ascii-code.com/">ascii-code.com</a></p>
 
     </div>
     <div class="col-2"></div>
