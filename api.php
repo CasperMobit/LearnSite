771b6611cee0ee3649b19d65a0ea6322cbbc4606
CasperMobit <casper.tollefsen@mobit.no>
2021-01-02 16:21:50 +0100

    Added content to API page

--- api.php
+++ api.php
@@ -8,15 +8,28 @@
 
 <?php include('./components/header.html'); ?>
 
-<div class="row" id="wrapper">
-    <div class="col-2"></div>
-    <div class="col-8 content">
-
-        <h1>API Guide</h1>
-        
+<div id="wrapper">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="heroTitle">API Guide</h1>
+        </div>
     </div>
-    <div class="col-2"></div>
-</div>    
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8 content noTopMargin">
+            <h1>Styling</h1>
+            <p>The website is styled using <abbr title="Syntactically Awesomes Style Sheets">Sass</abbr>.</p>
 
+            <h2>Color</h2>
+            <p>Use the <code>Color()</code> function to assign colors.</p>
+
+            <h1>Not categorized</h1>
+            <h2>Code Examples</h2>
+            <p>Insert inline code within the <code>&lt;code&gt;</code> and <code>&lt;&#47;code&gt;</code> tags.</p>
+            <p>Insert blocks of code within the <code>&lt;div&#32;class&#61;&quot;codeBlock&quot;&gt;</code> and <code>&lt;&#47;div&gt;</code> tags.</p>
+        </div>
+        <div class="col-2"></div>
+    </div>
+</div>
 </body>
 </html>
