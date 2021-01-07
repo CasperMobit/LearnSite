36e4749989b6c294f9143f32474aee98bb674deb
CasperMobit <casper.tollefsen@mobit.no>
2021-01-07 11:32:17 +0100

    Made main.js code more readable

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,38 @@
+"use strict";
+
 function includeHTML() {
-    var z, i, elmnt, file, xhttp, tmp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute("include-file");
+    var elmnt, elmntContent, elmntList, file, i, targetAttr, xhttp;
+    targetAttr = "include-file"; // The attribute to look for (target attribute)
+    elmntList = document.getElementsByTagName("*"); // Array containing every HTML element in document
+
+    // Loop through every element in the list
+    for (i = 0; i < elmntList.length; i++) {
+        elmnt = elmntList[i];
+
+        // See if current element has the target attribute
+        file = elmnt.getAttribute(targetAttr);
         if (file) {
-            tmp = elmnt.innerHTML; // Store code already present within element
-            /* Make an HTTP request using the attribute value as the file name: */
+            elmntContent = elmnt.innerHTML; // Store code already present within the element
+
+            // Make an HTTP request using the attribute value as the file name
             xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4) {
-                    if (this.status == 200) {elmnt.innerHTML = this.responseText + tmp;} // +tmp inserts code already present within element
-                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                    /* Remove the attribute, and call this function once more: */
-                    elmnt.removeAttribute("include-file");
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText + elmntContent; // Insert code that was already present within the element
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Content unavalable";
+                        alert(file + " was not found");
+                    }
+                    // Remove the attribute and call this function again
+                    elmnt.removeAttribute(targetAttr);
                     includeHTML();
                 }
             }
             xhttp.open("GET", file, true);
             xhttp.send();
-            file.innerHTML += tmp;
-            /* Exit the function: */
+            file.innerHTML += elmntContent; // Insert code that was already present within the element
             return;
         }
     }
